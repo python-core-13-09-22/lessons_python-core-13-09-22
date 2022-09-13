af9a7f77c3e9a4a35647105004aef7df27eaef9b
Liubomyr Halamaha <galamagal86@gmail.com>
2022-09-13 18:47:18 +0300

    added lesson01

---
+++ lesson01/lesson01.py
@@ -0,0 +1,55 @@
+A = 1
+a = 2
+
+
+# #pascal
+# function A ():int
+# begin
+#
+# end
+#
+# #c++
+# int function A()
+# {
+# int a;
+#   a = b+
+# c;
+# }
+def A():
+    print("a")
+    print("b")
+
+
+a = 1
+A()
+A()
+A()
+
+for i in range(5):
+    b = i\
+        ** i
+    print(i, b)
+c = 1
+
+# myLife
+# my_life! = 1
+
+# a_124_dhsvbfjs_1 = 2
+# 1a_124_dhsvbfjs_1 = 2
+
+a = 1
+b = 2
+print(a == b)
+print(a != b)
+print(a != b)
+
+import fibo
+fibo.fib(8)
+print(fibo.fib2(10))
+
+# from my.fibo import fib2
+# from my import fibo
+# fibo.fib2()
+# from my.foo import fibo
+# from my.foo.fibo import fib, fib2
+# from my.foo.fibo import *
