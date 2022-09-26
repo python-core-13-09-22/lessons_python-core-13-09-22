92180dd0fca7c1b15b0f06fd415799599e405729
Markiian <markiyanpavlychak@gmail.com>
2022-09-26 21:35:59 +0300

    hw03/2

--- lesson03/hw/Markiian/1.py
+++ lesson03/hw/Markiian/1.py
@@ -1,14 +1,16 @@
 import random
+import this
 # Python Zen
+x = str(this)
 
 pythonZen = "Beautiful is better than ugly. Explicit is better than implicit. Simple is better than complex. Complex is better than complicated. Flat is better than nested. Sparse is better than dense. Readability counts. Special cases aren't special enough to break the rules. Although practicality beats purity. Errors should never pass silently. Unless explicitly silenced. In the face of ambiguity, refuse the temptation to guess. There should be one -- and preferably only one -- obvious way to do it. Although that way may not be obvious at first unless you're Dutch. Now is better than never.    Although never is often better than *right* now. If the implementation is hard to explain, it's a bad idea. If the implementation is easy to explain, it may be a good idea. Namespaces are one honking great idea -- let's do more of those!  Live Python training"
-x1 = pythonZen.count('never')
+x1 = x.count('never')
 x2 = pythonZen.count('better')
 x3 = pythonZen.count('is')
 x4 = pythonZen.upper()
 x5 = pythonZen.replace('and', '&')
 
-# print(x1,x2,x3,x4,x5)
+print(x1,x2,x3)
 
 # XXXX number
 
