42b36f837dc7eae21da8d8c61198bc1c9fe644e8
Vasyl Vasyliv <vasuliv12@gmail.com>
2022-09-30 03:28:03 +0300

    New commit

---
+++ lesson04/hw/Vasyliv12/1.py
@@ -0,0 +1,34 @@
+# Високосний рік
+
+#r = int(input("Введіть рік: "))
+#print("This is not a leap year!") if r%4 != 0 else print("It's a leap year!")
+
+#Ввести три значення (рік, місяць, день) у відповідні змінні.
+
+#while True:
+#    r = input("Введіть рік:  ")
+#    if r.isdigit() and r.isnumeric() and len(r) < 5 and int(r)>0:
+#       break
+#    else:
+#        print("Ви ввели не правильне значення! Спробуйте ще ;) ")
+
+#while True:
+#    m = input("Введіть місяць:  ")
+#    if m.isdigit() and m.isnumeric() and len(m) < 3 and 12 >= int(m) > 0 :
+#       break
+#    else:
+#        print("Ви ввели не правильне значення! Спробуйте ще ;) ")
+#
+#while True:
+#    d = input("Введіть день: ")
+#    if d.isdigit() and d.isnumeric() and len(d) < 3 and 31 >= int(d) > 0 :
+#       break
+#    else:
+#        print("Ви ввели не правильне значення! Спробуйте ще ;) ")
+#
+#print(f"Дата {r}.{m}.{d} ")
+#
+
+
+# визначити, чи має рівняння `ax2+bx+c=0` хоча б один дійсний корінь
+
