c5fe24e0fbb06673d4c7051146bd6a631a377400
Markiian <markiyanpavlychak@gmail.com>
2022-10-25 18:07:08 +0300

    changes in hw09

--- lesson09/hw/Nyree911/hw09.py
+++ lesson09/hw/Nyree911/hw09.py
@@ -73,57 +73,75 @@ class CheatSheet():
     def __init__ (self, subject, number):
         self.subject = subject
         self.number = number
-    def __str__ (self):
-        return f"{self.subject} subject cheetsheet, number {self.number}"
+
+
 class PaperCheatSheet(CheatSheet):
-    def __init__(self, size, hide):
+    def __init__(self, subject, number, size, hide):
+        super().__init__(subject, number)
         self.size = size
         self.hide = hide
     def __str__ (self):
-        return f" {self.size} size, hiidden {self.hide}"
+        return f"Cheat Sheet on {self.subject}, number {self.number}, paper type, {self.size} size, hiidden {self.hide}"
+
 class ElectronicCheatSheet(CheatSheet):
-    def __init__ (self, hub):
+    def __init__ (self, subject, number, hub):
+        super().__init__(subject, number)
         self.hub = hub
+
     def __str__(self):
-        return f" on {self.hub}"
+        return f"Cheat Sheet on {self.subject}, number {self.number}, electronic type stored on {self.hub}"
+
 # Визначити тип «`Студент`»(ПІП, група, список шпаргалок)
 class Student(CheatSheet):
-    def __init__(self, name, group, cheatSheet, type_of_cheat):
+    def __init__(self, name, group, cheatSheet):
         self.name = name
         self.group = group
         self.lst_CheatSheet = cheatSheet
-        self.type_of_cheat = type_of_cheat
+
     def __str__(self):
-        return f"{self.name}, from {self.group} group, with {self.lst_CheatSheet},{self.type_of_cheat}"
+        return f"{self.name}, from {self.group} group, with {self.lst_CheatSheet}"
+
+    def ShowCheatStr(self):
+        return f"{self.lst_CheatSheet}"
+
     def ShowCheat(self):
-        return f'{self.lst_CheatSheet},{self.type_of_cheat}'
+        return self.lst_CheatSheet
 # Створити список студентів та додати їм шпаргалки різних типів.
 lst_students = []
+
 students = ["Mark", "Anthony", "Andrew", "Stewen", "Nicola", "Micky", "Jim", "David", "Angela", "Suzana", "Pamela", "Stefany"]
 subjects = ["Math", "Biology", "English", "Ukrainian", "Geography", "Science", "Chemesry"]
 places_of_hide = ["in pocket", "in socks", "in sleeves", "under the table"]
 hubs = ["mobile", "tablet", "laptop", "iWatch", "Samsung Watch"]
 size = ["big", 'medium', 'small']
-for _ in range (5):
-    lst_students.append(Student(students[randrange(0,11)], randrange(1,20), CheatSheet(subjects[randrange(0,6)], randrange(0,50)), PaperCheatSheet (size[randrange(0,2)], places_of_hide[randrange(0,4)])))
-for _ in range (5):
-    lst_students.append(Student(students[randrange(0,11)], randrange(1,20), CheatSheet(subjects[randrange(0,6)], randrange(0,50)), ElectronicCheatSheet (hubs[randrange(0,4)])))
+
+for _ in range (randrange(0,10)):
+    lst_students.append(Student(students[randrange(0,11)], randrange(1,20), PaperCheatSheet (subjects[randrange(0,6)], randrange(0,50), size[randrange(0,2)], places_of_hide[randrange(0,4)])))
+for _ in range (randrange(0,10)):
+    lst_students.append(Student(students[randrange(0,11)], randrange(1,20), ElectronicCheatSheet (subjects[randrange(0,6)], randrange(0,50), hubs[randrange(0,4)])))
 for i in lst_students:
     print(i)
+
 # Видрукувати список всіх шпаргалок, посортованих за предметом та номерами. 
+
 all_cheatSheets = []
 for stud in lst_students:
     all_cheatSheets.append(stud.ShowCheat())
-all_cheatSheets.sort()
-for i in all_cheatSheets:
+sort = sorted(all_cheatSheets, key = lambda x: (x.subject, x.number))
+for i in sort:
     print (i)
-# Вивести всі електронні шпаргалки на носіях «комп» та їх кількість.
+
+# # Вивести всі електронні шпаргалки на носіях «комп» та їх кількість.
 laptop_cheat = []
+all_cheatSheets = []
+for stud in lst_students:
+    all_cheatSheets.append(stud.ShowCheatStr())
 count = 0
-for sheet in all_cheatSheets:
+for sheet in all_cheatSheets: 
     if "laptop" in sheet:
         laptop_cheat.append(sheet)
         count+=1
-print(laptop_cheat)
+if count > 0:
+    print(laptop_cheat)
 print(f"There is {count} cheat sheets on laptop")
 
